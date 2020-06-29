47e8662f33c2777d46329f8c4c07300cf949840d
sndrjhlncgr <sandrocagaragithub@gmail.com>
2020-06-29 13:48:20 +0800

    adding processing video view

--- src/components/types/videoList.jsx
+++ src/components/types/videoList.jsx
@@ -18,7 +18,8 @@ class VideoList extends Component {
         type:'',
         size: 0,
         quality: '',
-        processing: false
+        processing: false,
+        processingVideoType: ''
     }
     
     render() {
@@ -33,7 +34,7 @@ class VideoList extends Component {
                     </td>
                 </tr>  */}
                 {this.props.info && this.props.info.formats && this.props.info.formats.filter(data =>  data.codecs.includes('H.264') || data.codecs.includes('av01') && data.mimeType.includes('video/mp4')).map((format, index) => {
-                        if(this.state.quality !== format.qualityLabel)  {
+                        if(this.state.quality !== format.qualityLabel && this.state.processingVideoType !== format.qualityLabel )  {
                             return (
                                 <tr key={index} className="video-formats">
                                     <td className="resolution-types">
@@ -51,7 +52,7 @@ class VideoList extends Component {
                                         <button type="button" className="btn btn-success btn-sm download-button" onClick={e => {
                                                     e.preventDefault();
                                                     const {url, info} = this.props
-                                                    this.setState({processing: true})
+                                                    this.setState({processing: true, processingVideoType:format.qualityLabel})
                                                     downloadMp4(url,format,info, (res) => {
                                                         switch(res.data.type) {
                                                             case 'MERGE_AUDIO_AND_VIDEO_SUCCESSFULLY':
@@ -61,14 +62,28 @@ class VideoList extends Component {
                                                                     type: res.data.type,
                                                                     size: res.data.size,
                                                                     quality: res.data.quality,
-                                                                    processing: false
+                                                                    processing: false,
+                                                                    processingVideoType: ''
                                                                 })
                                                                 break;
                                                             default:
-                                                                return ''
+                                                                return
                                                         }
                                                     })
                                             }}>Convert <FontAwesomeIcon className="ml-1" icon={faChevronCircleDown}/></button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+
+                        if(this.state.processingVideoType === format.qualityLabel)   {
+                            return (
+                                <tr key={index}>
+                                    <td colSpan="6">
+                                        <button className="form-control btn btn-sm btn-light processing-view">
+                                            <Spinner animation="border" className="convert-spinner" variant="secondary" />
+                                            <small className="ml-2">Converting video</small>
+                                        </button>
                                     </td>
                                 </tr>
                             )
@@ -79,9 +94,8 @@ class VideoList extends Component {
                                 <td colSpan="6">
                                     <button className="form-control btn btn-sm btn-success direct-download-link" onClick={(e) => {
                                         e.preventDefault()
-                                        const {link } = this.state
+                                        const { link } = this.state
                                         downloadVideo(link)
-                                        // window.location.href= `http://localhost:3000/api/download/video/${this.state.filename}`
                                     }}>Click to Download</button>
                                 </td>
                             </tr>
